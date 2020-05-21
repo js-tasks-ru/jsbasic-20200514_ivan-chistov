a7ebc569ae32a36e890859f55407defb95219e61
Ivan Chistov <chistov.ivan107@gmail.com>
2020-05-21 18:43:16 +0300

    занятие 1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+
+	if(!name || name.length<4 || name.includes(` `)){
+		return false;
+	} 
+	return true;
 }
 
 function sayHello() {
@@ -21,3 +26,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+
