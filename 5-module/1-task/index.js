e98f510cf1d428ecfa7956f74ad3ee4977019b4b
Ivan Chistov <chistov.ivan107@gmail.com>
2020-06-03 01:39:03 +0300

    Сделано задание 5.1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,61 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+	let rows = [...table.rows].slice(1);
+	//console.log(rows);
+
+
+
+	for(let row of rows){
+		//console.log( isRowAvailable(row) );
+		if (isRowAvailable(row) === true) {
+			row.classList.add('available');
+			//console.log(row.classList);
+		} else if (isRowAvailable(row) === false) {
+			row.classList.add('unavailable');
+		} else if (isRowAvailable(row) === null) {
+			row.hidden = true;
+		}
+
+
+		if (getRowsGender(row) === 'm') {
+			row.classList.add('male');
+		} else if (getRowsGender(row) === 'f') {
+			row.classList.add('female');
+		}
+
+
+		if (getRowsAge(row) < 18) {
+			row.style.textDecoration = 'line-through';
+		}
+	}
+	
+	return;
+
+}
+
+
+function isRowAvailable (row) {
+	
+	if(!row.cells[row.cells.length-1].dataset.available){
+		return null;
+	}
+
+	if(row.cells[row.cells.length-1].dataset.available === 'true'){
+		return true;
+	}
+
+	if(row.cells[row.cells.length-1].dataset.available === 'false'){
+		return false;
+	} 
+}
+
+function getRowsGender (row) {
+	//console.log(row.cells[2].innerHTML);
+	return row.cells[2].innerHTML;
+}
+
+function getRowsAge (row) {
+	return +row.cells[1].innerHTML;
 }
